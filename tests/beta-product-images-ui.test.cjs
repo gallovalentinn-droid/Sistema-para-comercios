2104c26e56a318bba4f78a14c46fbdb79e7c87e8
gallovalentinn-droid <gallovalentinn@gmail.com>
2026-09-13 12:34:56 -0300

    fix: publish F6 revision 12 invoice reader

--- tests/beta-product-images-ui.test.cjs
+++ tests/beta-product-images-ui.test.cjs
@@ -8,9 +8,9 @@ const artifactPath = path.resolve(__dirname, '../beta/index.html');
 const serviceWorkerPath = path.resolve(__dirname, '../beta/sw.js');
 const html = () => fs.readFileSync(artifactPath, 'utf8');
 
-test('el cache offline publicado corresponde a REV11', () => {
+test('el cache offline publicado corresponde a REV12', () => {
   const serviceWorker = fs.readFileSync(serviceWorkerPath, 'utf8');
-  assert.match(serviceWorker, /micomercio-beta-6\.0\.0-f6-rc2-rev11/);
+  assert.match(serviceWorker, /micomercio-beta-6\.0\.0-f6-rc2-rev12/);
 });
 
 function sliceBetween(source, start, end) {
