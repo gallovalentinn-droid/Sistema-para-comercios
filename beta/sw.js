851b4d84d51f6463d1e3bebba4c553bfbd80517f
gallovalentinn-droid <gallovalentinn@gmail.com>
2026-09-14 23:24:26 -0300

    fix: allow employees past owner onboarding gate

--- beta/sw.js
+++ beta/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX='micomercio-beta-';
-const CACHE='micomercio-beta-6.0.0-f6-rc2-rev14';
+const CACHE='micomercio-beta-6.0.0-f6-rc2-rev15';
 const BASE=['./','./index.html'];
 
 const EXTERNOS = [
